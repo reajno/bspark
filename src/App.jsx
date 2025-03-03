34ace4a6b782098a1ffa15715f6a7d4f1734f6a6
ajr <reanojohn@gmail.com>
2025-03-03 21:13:56 +1000

    refactor: use Map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import transformData from "../src/functions/transformData";
+import Map from "./components/Map";
 
 function App() {
-  const handleFetch = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("/api/getParkingData");
-      const data = await response.json();
-      const result = await transformData(data);
-      console.log(result);
-    } catch (error) {
-      throw error;
-    }
-  };
-
   return (
     <>
       <h1>bspark</h1>
       <div>
-        <button onClick={handleFetch}>Fetch Data</button>
+        <Map />
       </div>
     </>
   );
