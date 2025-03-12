b40f2964f50488c214f99f256d5e5236b4d92cc8
ajr <reanojohn@gmail.com>
2025-03-12 17:41:05 +1000

    feat: fetch parking locations based on destination lat/lng

--- src/hooks/useParkingData.js
+++ src/hooks/useParkingData.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import transformData from "../functions/transformData";
 
-const useParkingData = () => {
+const useParkingData = (latitude, longitude) => {
   const [loading, setLoading] = useState(true);
   const [parkingLocations, setParkingLocations] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!latitude || !longitude) return;
+
     const fetchData = async () => {
+      setLoading(true);
       try {
-        const response = await fetch("/api/getParkingData");
+        const response = await fetch(
+          `/api/getParkingData?latitude=${latitude}&longitude=${longitude}`
+        );
         const data = await response.json();
         const result = await transformData(data);
+
         setParkingLocations(result);
       } catch (error) {
         setError(error);
@@ -22,7 +28,7 @@ const useParkingData = () => {
     };
 
     fetchData();
-  }, []);
+  }, [latitude, longitude]);
 
   return { parkingLocations, loading, error };
 };
