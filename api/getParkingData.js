d1cefa20e9e1ab8fa38ed41fa45c7588309c94fd
ajr <reanojohn@gmail.com>
2025-03-12 17:35:27 +1000

    feat: add distance query

--- api/getParkingData.js
+++ api/getParkingData.js
@@ -1,6 +1,14 @@
 const handler = async (req, res) => {
   try {
-    const response = await fetch(process.env.API_URL);
+    const { latitude, longitude } = req.query;
+
+    const response =
+      await fetch(`https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/brisbane-parking-meters/records?
+      select=meter_no,street,suburb,restrictions,operational_day,operational_time,loc_desc,veh_bays,max_stay_hrs,latitude,longitude,
+      distance(geo_point_2d,GEOM'POINT(${longitude} ${latitude})') as distance
+      &order_by=distance(geo_point_2d,GEOM'POINT(${longitude} ${latitude})')
+      &limit=10
+      &having=distance < 1000`);
     const { results } = await response.json();
     res.status(200).json(results);
   } catch (error) {
