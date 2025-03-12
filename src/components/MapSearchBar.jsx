3ec1b3f237366e22045a70fba92a811094c2030e
ajr <reanojohn@gmail.com>
2025-03-12 17:37:12 +1000

    feat: add destination search bar

---
+++ src/components/MapSearchBar.jsx
@@ -0,0 +1,43 @@
+import Radar from "radar-sdk-js";
+import { useEffect, useRef } from "react";
+import useMarkers from "../hooks/useMarkers";
+
+const MapSearchBar = ({ mapRef }) => {
+  const autocompleteRef = useRef(null);
+
+  const { addDestinationMarker } = useMarkers(mapRef);
+
+  useEffect(() => {
+    const renderMapSearchBar = () => {
+      if (mapRef.current && autocompleteRef.current) {
+        Radar.ui.autocomplete({
+          container: autocompleteRef.current,
+          showMarkers: true,
+          markerColor: "#ACBDC8",
+          responsive: true,
+          width: "600px",
+          maxHeight: "600px",
+          placeholder: "Search address",
+          limit: 8,
+          minCharacters: 3,
+          near: { latitude: -27.4698, longitude: 153.0251 },
+          layers: ["place", "address", "coarse"],
+          countryCode: "AU",
+          onSelection: (destination) => {
+            addDestinationMarker(destination);
+          },
+        });
+      }
+    };
+
+    renderMapSearchBar();
+  }, []);
+
+  return (
+    <div
+      ref={autocompleteRef}
+      style={{ display: "flex", justifyContent: "center" }}
+    />
+  );
+};
+export default MapSearchBar;
