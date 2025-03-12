bb15e35d6dc5a271b1b14a70661bfef0347b001f
ajr <reanojohn@gmail.com>
2025-03-12 17:36:37 +1000

    chore: refactor and clean up code

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,40 +1,35 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import Radar from "radar-sdk-js";
 import "radar-sdk-js/dist/radar.css";
 
-import useParkingData from "../hooks/useParkingData";
-import useMarkers from "../hooks/useMarkers";
+import MapSearchBar from "./MapSearchBar";
 
+// CHANGE API_KEY LOCATION TO ENV
 const API_KEY = "prj_test_pk_cb82d2a47c0d6296bf414c4936df9e2e8630234d";
 
-const MapRadar = () => {
-  const mapContainerRef = useRef(null);
-  const map = useRef(null);
-
-  const { parkingLocations } = useParkingData();
-  const { addMarkers, clearMarkers } = useMarkers(map);
+const Map = () => {
+  const mapRef = useRef(null);
 
   useEffect(() => {
     const initializeMap = () => {
       Radar.initialize(API_KEY);
 
-      if (mapContainerRef.current && !map.current) {
-        map.current = Radar.ui.map({
-          container: mapContainerRef.current,
-          style: "radar-default-v1",
-          center: { lat: -27.4698, lng: 153.0251 },
-          zoom: 12,
-        });
+      const map = Radar.ui.map({
+        container: mapRef.current,
+        style: "radar-default-v1",
+        center: { lat: -27.4698, lng: 153.0251 },
+        zoom: 12,
+      });
+      // REMOVE DEFAULT NAVIGATION
+      const navControl = map._controls.find(
+        (control) =>
+          control._container &&
+          control._container.classList.contains("maplibregl-ctrl") &&
+          control._container.classList.contains("maplibregl-ctrl-group")
+      );
+      if (navControl) map.removeControl(navControl);
 
-        // REMOVE DEFAULT NAVIGATION
-        const navControl = map.current._controls.find(
-          (control) =>
-            control._container &&
-            control._container.classList.contains("maplibregl-ctrl") &&
-            control._container.classList.contains("maplibregl-ctrl-group")
-        );
-        if (navControl) map.current.removeControl(navControl);
-      }
+      mapRef.current = map;
     };
 
     initializeMap();
@@ -42,10 +37,9 @@ const MapRadar = () => {
 
   return (
     <>
-      <div ref={mapContainerRef} style={{ width: "100%", height: "500px" }} />
-      <button onClick={() => addMarkers(parkingLocations)}>Add Markers</button>
-      <button onClick={clearMarkers}>Clear Markers</button>
+      <div ref={mapRef} style={{ width: "100%", height: "500px" }} />
+      <MapSearchBar mapRef={mapRef} />
     </>
   );
 };
-export default MapRadar;
+export default Map;
